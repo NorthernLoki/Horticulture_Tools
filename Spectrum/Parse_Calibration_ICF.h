f3fef3c65c2e0b37b9bbe5c7238b536c9928c221
NorthernLoki <55947516+NorthernLoki@users.noreply.github.com>
2019-09-29 07:55:27 -0400

    Create Parse_Calibration_ICF.h

---
+++ Spectrum/Parse_Calibration_ICF.h
@@ -0,0 +1,36 @@
+//
+// Parse_Calibration_ICF.h
+// Version timestamp: 12-11-2018, 4:27 PM
+//
+// Attribution : Copyright (c) 2018 Northern_Loki (sha256::6F290BF833967127BE26C92C8F6B1C1A3949C55A7EABCEF3ECC785CD2D38D30D)
+// License is granted under the Creative Commons Attribution-ShareAlike 4.0 International.  https://creativecommons.org/licenses/by-sa/4.0/
+//
+#pragma once
+#include <fstream>      
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>  
+#include <boost/tokenizer.hpp>
+#include <boost/unordered_map.hpp>
+#include "spectral_data.h"
+
+class Parse_Calibration_ICF : public spectral_data
+{
+public:
+    Parse_Calibration_ICF();
+    Parse_Calibration_ICF(std::string &filepath);
+    ~Parse_Calibration_ICF();
+    
+	void set(std::string &filepath);
+    float get_intensity(float wavelength);
+    std::pair<float, float> get_max_intensity();
+    std::pair<float, float> get_min_intensity();
+    std::pair<float, float> get_range();
+    unsigned int size();
+    std::map<float, float>::iterator begin();
+    std::map<float, float>::iterator end();
+	virtual void operator()(std::string &filepath);
+    
+    spectral_data ordered_spectra;
+};
