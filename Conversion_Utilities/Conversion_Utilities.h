6be6b60c056206dc6724726c49044eeaa4e7f30b
Northern_Loki <loki@northernloki.com>
2020-02-10 08:20:59 -0500

    Update Conversion_Utilities.h
    Added Rankine units.
    Set the base class to Celcius such that any mathematical operations using the temperature types are always performed in Celcius and then converted back to the target temperature unit. Math with temperature, in some cases, is nonsensical since we should be talking temperature deltas. Though, as a class, mathematical formulas can utilize the conversions allowing for flexibility. Just be aware that additions between two different units will always be converted to Celcius for the operation, first.
    Refactored code such that temperature types are preserved internally. Previously, everything was converted to double float internally.
    Fixed speeling error on faranheit.

--- Conversion_Utilities/Conversion_Utilities.h
+++ Conversion_Utilities/Conversion_Utilities.h
@@ -1,3 +1,10 @@
+//
+// Conversion_Utilities.h
+// Version timestamp: 2-9-2020, 7:50 PM
+//
+// Attribution : Copyright (c) 2018 Northern_Loki (sha256::6F290BF833967127BE26C92C8F6B1C1A3949C55A7EABCEF3ECC785CD2D38D30D)
+// License is granted under the Creative Commons Attribution-ShareAlike 4.0 International.  https://creativecommons.org/licenses/by-sa/4.0/
+//
 #pragma once
 #include "temperature_types.h"
 #include "power_types.h"
@@ -10,8 +17,9 @@
 extern "C" {
 #endif
 
-int Conversion_UtilitiesTest();
+    int Conversion_UtilitiesTest();
 
 #ifdef __cplusplus
 }
 #endif
+
