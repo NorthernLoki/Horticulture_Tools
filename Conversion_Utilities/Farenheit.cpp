155f201d6f80668d01900509caa4e7d5e17dba85
Northern_Loki <loki@northernloki.com>
2020-02-10 08:22:04 -0500

    Update Farenheit.cpp
    Added Rankine units.
    Set the base class to Celcius such that any mathematical operations using the temperature types are always performed in Celcius and then converted back to the target temperature unit. Math with temperature, in some cases, is nonsensical since we should be talking temperature deltas. Though, as a class, mathematical formulas can utilize the conversions allowing for flexibility. Just be aware that additions between two different units will always be converted to Celcius for the operation, first.
    Refactored code such that temperature types are preserved internally. Previously, everything was converted to double float internally.
    Fixed speeling error on faranheit.

--- Conversion_Utilities/Farenheit.cpp
+++ Conversion_Utilities/Farenheit.cpp
@@ -1,133 +1,203 @@
 //
-// Farenheit.cpp
-// Version timestamp: 9-4-2018, 4:23 PM
+// fahrenheit.cpp
+// Version timestamp: 2-9-2020, 1:33 PM
 //
 // Attribution : Copyright (c) 2018 Northern_Loki (sha256::6F290BF833967127BE26C92C8F6B1C1A3949C55A7EABCEF3ECC785CD2D38D30D)
 // License is granted under the Creative Commons Attribution-ShareAlike 4.0 International.  https://creativecommons.org/licenses/by-sa/4.0/
 //
 #include "temperature_types.h"
 
-farenheit::farenheit(farenheit_variant &r)
+fahrenheit::fahrenheit(fahrenheit_variant &r)
 {
-	farenheit_variant c = r;
-	this->degrees_farenheit = boost::apply_visitor(tfarenheit_variant(), c);
+    fahrenheit_variant c = r;
+    
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), c);
+    this->degrees_fahrenheit = temp.degrees_fahrenheit;
 }
 
-farenheit::operator kelvin()
+fahrenheit::operator kelvin()
 {
-	//boost::apply_visitor(tcelcius_variant(), this);
-	double temp = (this->degrees_farenheit - 32.0) * (5.0 / 9.0) + 273.15;
-	return (temp);
+    kelvin temp = (this->degrees_fahrenheit - 32.0) / (1.8) + 273.15;
+    return (temp);
 }
 
-farenheit::operator celcius()
+fahrenheit::operator celcius()
 {
-	double temp = (this->degrees_farenheit - 32.0) * (5.0 / 9.0);
-	return (temp);
+    celcius temp = (this->degrees_fahrenheit - 32.0) / (1.8);
+    return (temp);
 }
 
-void farenheit::operator=(farenheit_variant &temp2) 
+fahrenheit::operator rankine()
 {
-	this->degrees_farenheit = boost::apply_visitor(tfarenheit_variant(), temp2);
+    rankine temp = (this->degrees_fahrenheit + 459.67);
+    return (temp);
 }
 
-double farenheit::operator-(farenheit_variant temp2) 
+void fahrenheit::operator=(fahrenheit_variant &temp2) 
 {
-	double temp = this->degrees_farenheit - boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (temp);
+    fahrenheit temp = boost::apply_visitor(tcelcius_variant(), temp2);
+    this->degrees_fahrenheit = temp.degrees_fahrenheit;
 }
 
-double farenheit::operator+(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator-(fahrenheit_variant temp2) 
 {
-	double temp = this->degrees_farenheit + boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (temp);
+    celcius temp = celcius(*this);
+    temp -= boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator*(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator+(fahrenheit_variant temp2) 
 {
-	double temp = this->degrees_farenheit * boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (temp);
+    celcius temp = celcius(*this);
+    temp += boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator/(farenheit_variant temp2) 
+fahrenheit operator+(const double &c1, fahrenheit c2)
 {
-	double temp = this->degrees_farenheit / boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (temp);
+    celcius temp = c1 + celcius(c2);
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator()(farenheit_variant temp2) 
+fahrenheit operator*(const double &c1, fahrenheit c2)
 {
-	double temp = boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (temp);
+    celcius temp = c1 * celcius(c2);
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator()() 
+fahrenheit operator/(const double &c1, fahrenheit c2)
 {
-	double temp = this->degrees_farenheit;
-	return (temp);
+    celcius temp = c1 / celcius(c2);
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator-=(farenheit_variant temp2) 
+fahrenheit operator-(const double &c1, fahrenheit c2)
 {
-	this->degrees_farenheit = this->degrees_farenheit - boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (this->degrees_farenheit);
+    celcius temp = c1 - celcius(c2);
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator+=(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator*(fahrenheit_variant temp2) 
 {
-	this->degrees_farenheit = this->degrees_farenheit + boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (this->degrees_farenheit);
+    celcius temp = celcius(*this);
+    temp *= boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator++(int incr) 
+fahrenheit fahrenheit::operator/(fahrenheit_variant temp2) 
 {
-	this->degrees_farenheit +=  1.0;
-	return (this->degrees_farenheit);
+    celcius temp = celcius(*this);
+    temp /= boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((fahrenheit)temp);
 }
 
-double farenheit::operator--(int incr) 
+double fahrenheit::operator()(fahrenheit_variant temp2) 
 {
-	this->degrees_farenheit -=  1.0;
-	return (this->degrees_farenheit);
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    return (temp.degrees_fahrenheit);
 }
 
-double farenheit::operator*=(farenheit_variant temp2) 
+double fahrenheit::operator()() 
 {
-	this->degrees_farenheit = this->degrees_farenheit * boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (this->degrees_farenheit);
+    double temp = this->degrees_fahrenheit;
+    return (temp);
 }
 
-double farenheit::operator/=(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator-=(fahrenheit_variant temp2) 
 {
-	this->degrees_farenheit = this->degrees_farenheit / boost::apply_visitor(tfarenheit_variant(), temp2);
-	return (this->degrees_farenheit);
+    celcius temp = celcius(*this);
+    temp = temp - boost::apply_visitor(tcelcius_variant(), temp2);
+    fahrenheit tempf = temp;
+    this->degrees_fahrenheit = tempf.degrees_fahrenheit;
+    
+    return (tempf);
 }
 
-double farenheit::operator>(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator+=(fahrenheit_variant temp2) 
 {
-	if (this->degrees_farenheit > boost::apply_visitor(tfarenheit_variant(), temp2)) return (1);
-	return (0);
+    celcius temp = celcius(*this);
+    temp = temp + boost::apply_visitor(tcelcius_variant(), temp2);
+    fahrenheit tempf = temp;
+    this->degrees_fahrenheit = tempf.degrees_fahrenheit;
+    
+    return (tempf);
 }
 
-double farenheit::operator<(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator++(int incr) 
 {
-	if (this->degrees_farenheit < boost::apply_visitor(tfarenheit_variant(), temp2)) return (1);
-	return (0);
+    this->degrees_fahrenheit +=  1.0;
+    return (this->degrees_fahrenheit);
 }
 
-double farenheit::operator==(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator--(int incr) 
 {
-	if (this->degrees_farenheit == boost::apply_visitor(tfarenheit_variant(), temp2)) return (1);
-	return (0);
+    this->degrees_fahrenheit -=  1.0;
+    return (this->degrees_fahrenheit);
 }
 
-double farenheit::operator>=(farenheit_variant temp2) 
+fahrenheit fahrenheit::operator*=(fahrenheit_variant temp2) 
 {
-	if (this->degrees_farenheit >= boost::apply_visitor(tfarenheit_variant(), temp2)) return (1);
-	return (0);
+    celcius temp = celcius(*this);
+    temp = temp * boost::apply_visitor(tcelcius_variant(), temp2);
+    fahrenheit tempf = temp;
+    this->degrees_fahrenheit = tempf.degrees_fahrenheit;
+    
+    return (tempf);
 }
-double farenheit::operator<=(farenheit_variant temp2) 
+
+fahrenheit fahrenheit::operator/=(fahrenheit_variant temp2) 
 {
-	if (this->degrees_farenheit <= boost::apply_visitor(tfarenheit_variant(), temp2)) return (1);
-	return (0);
+    celcius temp = celcius(*this);
+    temp = temp / boost::apply_visitor(tcelcius_variant(), temp2);
+    fahrenheit tempf = temp;
+    this->degrees_fahrenheit = tempf.degrees_fahrenheit;
+    
+    return (tempf);
+}
+
+bool fahrenheit::operator>(fahrenheit_variant temp2) 
+{ 
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    if (this->degrees_fahrenheit > temp.degrees_fahrenheit) return (1);
+    return (0);
+}
+
+bool fahrenheit::operator<(fahrenheit_variant temp2) 
+{
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    if (this->degrees_fahrenheit < temp.degrees_fahrenheit) return (1);
+    return (0);
+}
+
+bool fahrenheit::operator==(fahrenheit_variant temp2) 
+{
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    if (fabs(temp.degrees_fahrenheit - this->degrees_fahrenheit) <= 
+    __DBL_EPSILON__ * fmax(fabs(temp.degrees_fahrenheit), fabs(this->degrees_fahrenheit))) return (1);
+    return (0);
+}
+
+bool fahrenheit::operator!=(fahrenheit_variant temp2) 
+{
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    if (fabs(temp.degrees_fahrenheit - this->degrees_fahrenheit) <= 
+    __DBL_EPSILON__ * fmax(fabs(temp.degrees_fahrenheit), fabs(this->degrees_fahrenheit))) return (0);
+    return (1);
+}
+
+bool fahrenheit::operator>=(fahrenheit_variant temp2) 
+{
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    if (this->degrees_fahrenheit >= temp.degrees_fahrenheit) return (1);
+    return (0);
+}
+bool fahrenheit::operator<=(fahrenheit_variant temp2) 
+{
+    fahrenheit temp = boost::apply_visitor(tfahrenheit_variant(), temp2);
+    if (this->degrees_fahrenheit <= temp.degrees_fahrenheit) return (1);
+    return (0);
 }
