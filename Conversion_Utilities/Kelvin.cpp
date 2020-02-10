a5b1638048bcefbff46db171283c783fd1b54e47
Northern_Loki <loki@northernloki.com>
2020-02-10 08:22:34 -0500

    Update Kelvin.cpp
    Added Rankine units.
    Set the base class to Celcius such that any mathematical operations using the temperature types are always performed in Celcius and then converted back to the target temperature unit. Math with temperature, in some cases, is nonsensical since we should be talking temperature deltas. Though, as a class, mathematical formulas can utilize the conversions allowing for flexibility. Just be aware that additions between two different units will always be converted to Celcius for the operation, first.
    Refactored code such that temperature types are preserved internally. Previously, everything was converted to double float internally.
    Fixed speeling error on faranheit.

--- Conversion_Utilities/Kelvin.cpp
+++ Conversion_Utilities/Kelvin.cpp
@@ -1,6 +1,6 @@
 //
 // Kelvin.cpp
-// Version timestamp: 9-4-2018, 4:23 PM
+// Version timestamp: 2-9-2020, 1:33 PM
 //
 // Attribution : Copyright (c) 2018 Northern_Loki (sha256::6F290BF833967127BE26C92C8F6B1C1A3949C55A7EABCEF3ECC785CD2D38D30D)
 // License is granted under the Creative Commons Attribution-ShareAlike 4.0 International.  https://creativecommons.org/licenses/by-sa/4.0/
@@ -9,125 +9,195 @@
 
 kelvin::kelvin(kelvin_variant &r)
 {
-	kelvin_variant c = r;
-	this->degrees_kelvin = boost::apply_visitor(tkelvin_variant(), c);
+    kelvin_variant c = r;
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), c);
+    this->degrees_kelvin = temp.degrees_kelvin;
 }
 
-kelvin::operator farenheit()
+kelvin::operator fahrenheit()
 {
-	//boost::apply_visitor(tcelcius_variant(), this);
-	double temp = (this->degrees_kelvin) * (9.0 / 5.0) - 459.67;
-	return (temp);
+    fahrenheit temp = (this->degrees_kelvin) * (1.8) - 459.67;
+    return (temp);
 }
 
 kelvin::operator celcius()
 {
-	double temp = this->degrees_kelvin - 273.15;
-	return (temp);
+    celcius temp = this->degrees_kelvin - 273.15;
+    return (temp);
 }
+
+kelvin::operator rankine()
+{
+    rankine temp = this->degrees_kelvin * (1.8);
+    return (temp);
+}
+
 
 void kelvin::operator=(kelvin_variant &temp2) 
 {
-	this->degrees_kelvin = boost::apply_visitor(tkelvin_variant(), temp2);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    this->degrees_kelvin = temp.degrees_kelvin;
 }
 
-double kelvin::operator-(kelvin_variant temp2) 
+kelvin kelvin::operator-(kelvin_variant temp2) 
 {
-	double temp = this->degrees_kelvin - boost::apply_visitor(tkelvin_variant(), temp2);
-	return (temp);
+    celcius temp = celcius(*this);
+    temp -= boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((kelvin)temp);
 }
 
-double kelvin::operator+(kelvin_variant temp2) 
+kelvin kelvin::operator+(celcius_variant temp2) 
 {
-	double temp = this->degrees_kelvin + boost::apply_visitor(tkelvin_variant(), temp2);
-	return (temp);
+    celcius temp = celcius(*this);
+    temp += boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((kelvin)temp);
 }
 
-double kelvin::operator*(kelvin_variant temp2) 
+kelvin operator+(const double &c1, kelvin c2)
 {
-	double temp = this->degrees_kelvin * boost::apply_visitor(tkelvin_variant(), temp2);
-	return (temp);
+    celcius temp = c1 + celcius(c2);
+    return ((kelvin)temp);
 }
 
-double kelvin::operator/(kelvin_variant temp2) 
+kelvin operator*(const double &c1, kelvin c2)
 {
-	double temp = this->degrees_kelvin / boost::apply_visitor(tkelvin_variant(), temp2);
-	return (temp);
+    celcius temp = c1 * celcius(c2);
+    return ((kelvin)temp);
+}
+
+kelvin operator/(const double &c1, kelvin c2)
+{
+    celcius temp = c1 / celcius(c2);
+    return ((kelvin)temp);
+}
+
+kelvin operator-(const double &c1, kelvin c2)
+{
+    celcius temp = c1 - celcius(c2);
+    return ((kelvin)temp);
+}
+
+kelvin kelvin::operator*(kelvin_variant temp2) 
+{
+    celcius temp = celcius(*this);
+    temp *= boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((kelvin)temp);
+}
+
+kelvin kelvin::operator/(kelvin_variant temp2) 
+{
+    celcius temp = celcius(*this);
+    temp /= boost::apply_visitor(tcelcius_variant(), temp2);
+    
+    return ((kelvin)temp);
 }
 
 double kelvin::operator()(kelvin_variant temp2) 
 {
-	double temp = boost::apply_visitor(tkelvin_variant(), temp2);
-	return (temp);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    return (temp.degrees_kelvin);
 }
 
 double kelvin::operator()() 
 {
-	double temp = this->degrees_kelvin;
-	return (temp);
+    double temp = this->degrees_kelvin;
+    return (temp);
 }
 
-double kelvin::operator-=(kelvin_variant temp2) 
+kelvin kelvin::operator-=(kelvin_variant temp2) 
 {
-	this->degrees_kelvin = this->degrees_kelvin - boost::apply_visitor(tkelvin_variant(), temp2);
-	return (this->degrees_kelvin);
+    celcius temp = celcius(*this);
+    temp = temp - boost::apply_visitor(tcelcius_variant(), temp2);
+    kelvin tempk = temp;
+    this->degrees_kelvin = tempk.degrees_kelvin;
+    
+    return (tempk);
 }
 
-double kelvin::operator+=(kelvin_variant temp2) 
+kelvin kelvin::operator+=(kelvin_variant temp2) 
 {
-	this->degrees_kelvin = this->degrees_kelvin + boost::apply_visitor(tkelvin_variant(), temp2);
-	return (this->degrees_kelvin);
+    celcius temp = celcius(*this);
+    temp = temp + boost::apply_visitor(tcelcius_variant(), temp2);
+    kelvin tempk = temp;
+    this->degrees_kelvin = tempk.degrees_kelvin;
+    
+    return (tempk);
 }
 
-double kelvin::operator++(int incr) 
+kelvin kelvin::operator++(int incr) 
 {
-	this->degrees_kelvin +=  1.0;
-	return (this->degrees_kelvin);
+    this->degrees_kelvin +=  1.0;
+    return (this->degrees_kelvin);
 }
 
-double kelvin::operator--(int incr) 
+kelvin kelvin::operator--(int incr) 
 {
-	this->degrees_kelvin -= 1.0;
-	return (this->degrees_kelvin);
+    this->degrees_kelvin -= 1.0;
+    return (this->degrees_kelvin);
 }
 
-double kelvin::operator*=(kelvin_variant temp2) 
+kelvin kelvin::operator*=(kelvin_variant temp2) 
 {
-	this->degrees_kelvin = this->degrees_kelvin * boost::apply_visitor(tkelvin_variant(), temp2);
-	return (this->degrees_kelvin);
+    celcius temp = celcius(*this);
+    temp = temp * boost::apply_visitor(tcelcius_variant(), temp2);
+    kelvin tempk = temp;
+    this->degrees_kelvin = tempk.degrees_kelvin;
+    
+    return (tempk);
 }
 
-double kelvin::operator/=(kelvin_variant temp2) 
+kelvin kelvin::operator/=(kelvin_variant temp2) 
 {
-	this->degrees_kelvin = this->degrees_kelvin / boost::apply_visitor(tkelvin_variant(), temp2);
-	return (this->degrees_kelvin);
+    celcius temp = celcius(*this);
+    temp = temp / boost::apply_visitor(tcelcius_variant(), temp2);
+    kelvin tempk = temp;
+    this->degrees_kelvin = tempk.degrees_kelvin;
+    
+    return (tempk);
 }
 
-double kelvin::operator>(kelvin_variant temp2) 
+bool kelvin::operator>(kelvin_variant temp2) 
 {
-	if (this->degrees_kelvin > boost::apply_visitor(tkelvin_variant(), temp2)) return (1);
-	return (0);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    if (this->degrees_kelvin > temp.degrees_kelvin) return (1);
+    return (0);
 }
 
-double kelvin::operator<(kelvin_variant temp2) 
+bool kelvin::operator<(kelvin_variant temp2) 
 {
-	if (this->degrees_kelvin < boost::apply_visitor(tkelvin_variant(), temp2)) return (1);
-	return (0);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    if (this->degrees_kelvin < temp.degrees_kelvin) return (1);
+    return (0);
 }
 
-double kelvin::operator==(kelvin_variant temp2) 
+bool kelvin::operator==(kelvin_variant temp2) 
 {
-	if (this->degrees_kelvin == boost::apply_visitor(tkelvin_variant(), temp2)) return (1);
-	return (0);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    if (fabs(temp.degrees_kelvin - this->degrees_kelvin) <= 
+    __DBL_EPSILON__ * fmax(fabs(temp.degrees_kelvin), fabs(this->degrees_kelvin))) return (1);
+    return (0);
 }
 
-double kelvin::operator>=(kelvin_variant temp2) 
+bool kelvin::operator!=(kelvin_variant temp2) 
 {
-	if (this->degrees_kelvin >= boost::apply_visitor(tkelvin_variant(), temp2)) return (1);
-	return (0);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    if (fabs(temp.degrees_kelvin - this->degrees_kelvin) <= 
+        __DBL_EPSILON__ * fmax(fabs(temp.degrees_kelvin), fabs(this->degrees_kelvin))) return (0);
+    return (1);
 }
-double kelvin::operator<=(kelvin_variant temp2) 
+
+bool kelvin::operator>=(kelvin_variant temp2) 
 {
-	if (this->degrees_kelvin <= boost::apply_visitor(tkelvin_variant(), temp2)) return (1);
-	return (0);
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    if (this->degrees_kelvin >= temp.degrees_kelvin) return (1);
+    return (0);
+}
+bool kelvin::operator<=(kelvin_variant temp2) 
+{
+    kelvin temp = boost::apply_visitor(tkelvin_variant(), temp2);
+    if (this->degrees_kelvin <= temp.degrees_kelvin) return (1);
+    return (0);
 }
